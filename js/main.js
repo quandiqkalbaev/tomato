efb3d5462c3e76ce326104a3b8fbdb21d1a0c1b5
quandiqdev <quandiqdev@gmail.com>
2023-10-06 03:04:23 +0500

    intro header info added

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 try {
   // Burger Menu
   const hamburger = document.querySelector(".hamburger");
-  const menu = document.querySelector(".header__menu");
+  const menu = document.querySelector(".header__nav");
   // const menuLinks = document.querySelectorAll(".header__nav-link");
 
   hamburger.addEventListener("click", (e) => {
     hamburger.classList.toggle("hamburger--active");
-    menu.classList.toggle("header__menu--active");
+    menu.classList.toggle("header__nav--active");
 
     if (hamburger.classList.contains("hamburger--active")) {
       document.body.classList.add("no-scroll");
