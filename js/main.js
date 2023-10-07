4841b03e2df8cea4d9313c33af277e1270f149d5
quandiqdev <quandiqdev@gmail.com>
2023-10-08 04:20:50 +0500

    github all

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // Burger Menu
 const hamburger = document.querySelector(".hamburger");
 const menu = document.querySelector(".header__nav");
-// const menuLinks = document.querySelectorAll(".header__nav-link");
+const menuLinks = document.querySelectorAll(".header__nav-link");
 
 hamburger.addEventListener("click", (e) => {
   hamburger.classList.toggle("hamburger--active");
@@ -9,6 +9,14 @@ hamburger.addEventListener("click", (e) => {
 
   if (hamburger.classList.contains("hamburger--active")) {
     document.body.classList.add("no-scroll");
+
+    menuLinks.forEach((link) => {
+      link.addEventListener("click", () => {
+        hamburger.classList.remove("hamburger--active");
+        menu.classList.remove("header__nav--active");
+        document.body.classList.remove("no-scroll");
+      });
+    });
   } else {
     document.body.classList.remove("no-scroll");
   }
@@ -42,18 +50,18 @@ function progressUpdate() {
   progress.value = (100 * c) / d;
 }
 playerPlay.addEventListener("click", (e) => {
-  video.classList.add('player__video--active')
+  video.classList.add("player__video--active");
   play();
   playerPlay.style.display = "none";
 });
 
 playerExit.addEventListener("click", () => {
-  video.classList.remove('player__video--active')
+  video.classList.remove("player__video--active");
   stop();
   playerPlay.style.display = "block";
 });
 video.addEventListener("ended", () => {
-  video.classList.remove('player__video--active')
+  video.classList.remove("player__video--active");
   playerPlay.style.display = "block";
   progress.style.display = "none";
 });
@@ -83,3 +91,252 @@ modals.forEach((e) => {
     document.body.classList.remove("no-scroll");
   });
 });
+
+try {
+  class Tab {
+    constructor(
+      tabSelector,
+      contentSelector,
+      activeTabClass,
+      hiddenContentClass
+    ) {
+      this.tabs = document.querySelectorAll(tabSelector);
+      this.contents = document.querySelectorAll(contentSelector);
+      this.activeTabClass = activeTabClass || "tabs__item--active";
+      this.hiddenContentClass = hiddenContentClass || "hide";
+      this.currentTabIndex = 0;
+
+      this.hideTabContent();
+      this.showTabContent(this.currentTabIndex);
+
+      this.tabs.forEach((tab, index) => {
+        tab.addEventListener("click", () => {
+          this.hideTabContent();
+          this.showTabContent(index);
+        });
+      });
+    }
+
+    hideTabContent() {
+      this.contents.forEach((content) => {
+        content.classList.add(this.hiddenContentClass);
+        content.classList.remove("show", "fade");
+      });
+
+      this.tabs.forEach((tab) => {
+        tab.classList.remove(this.activeTabClass);
+      });
+    }
+
+    showTabContent(index = 0) {
+      if (index >= 0 && index < this.contents.length) {
+        this.contents[index].classList.add("show", "fade");
+        this.contents[index].classList.remove(this.hiddenContentClass);
+        this.tabs[index].classList.add(this.activeTabClass);
+        this.currentTabIndex = index;
+      }
+    }
+  }
+
+  // Использование класса для создания табуляции
+  const recipeTab = new Tab(".recipe .tabs__item", ".recipe .tabs__catalog");
+
+  const assortmentTab = new Tab(
+    ".assortment .tabs__item",
+    ".assortment .tabs__catalog"
+  );
+
+  const buyTab = new Tab(".buy .tabs__item", ".buy .tabs__catalog");
+} catch (e) {}
+
+try {
+  class Tab {
+    constructor(
+      sliderSelector,
+      tabSelector,
+      contentSelector,
+      activeTabClass,
+      hiddenContentClass
+    ) {
+      this.sliders = document.querySelectorAll(sliderSelector);
+      this.tabSelector = tabSelector;
+      this.contentSelector = contentSelector;
+      this.activeTabClass = activeTabClass || "r-item__tabs-item--active";
+      this.hiddenContentClass = hiddenContentClass || "hide";
+
+      this.sliders.forEach((slider) => {
+        const tabs = slider.querySelectorAll(this.tabSelector);
+        const contents = slider.querySelectorAll(this.contentSelector);
+
+        tabs.forEach((tab, index) => {
+          tab.addEventListener("click", () => {
+            this.hideTabContent(slider);
+            this.showTabContent(slider, index);
+          });
+        });
+
+        this.hideTabContent(slider);
+        this.showTabContent(slider, 0);
+      });
+    }
+
+    hideTabContent(slider) {
+      const contents = slider.querySelectorAll(this.contentSelector);
+
+      contents.forEach((content) => {
+        content.classList.add(this.hiddenContentClass);
+        content.classList.remove("show", "fade");
+      });
+
+      const tabs = slider.querySelectorAll(this.tabSelector);
+      tabs.forEach((tab) => {
+        tab.classList.remove(this.activeTabClass);
+      });
+    }
+
+    showTabContent(slider, index = 0) {
+      const contents = slider.querySelectorAll(this.contentSelector);
+      const tabs = slider.querySelectorAll(this.tabSelector);
+
+      if (index >= 0 && index < contents.length) {
+        contents[index].classList.add("show", "fade");
+        contents[index].classList.remove(this.hiddenContentClass);
+        tabs[index].classList.add(this.activeTabClass);
+      }
+    }
+  }
+
+  // Использование класса для создания табуляции в слайдерах
+  const sliderTabs = new Tab(
+    ".r-item",
+    ".r-item__tabs-item",
+    ".r-item__tabs-catalog"
+  );
+} catch (e) {}
+
+try {
+  const assortmentSlider = new Swiper(".assortment__slider", {
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+    watchOverflow: true,
+    spaceBetween: 15,
+    navigation: {
+      nextEl: ".assortment__slider-next",
+      prevEl: ".assortment__slider-prev",
+    },
+    pagination: {
+      el: ".assortment__slider-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      768: {
+        slidesPerView: 1,
+        slidesPerGroup: 1,
+      },
+      992: {
+        slidesPerView: 2,
+        slidesPerGroup: 2,
+      },
+      1440: {
+        slidesPerView: 3,
+        slidesPerGroup: 3,
+      },
+      1920: {
+        slidesPerView: 4,
+        slidesPerGroup: 4,
+      },
+    },
+  });
+} catch (e) {}
+
+try {
+  const recipeSlider = new Swiper(".recipe__slider", {
+    slidesPerView: 1,
+    spaceBetween: 5,
+    navigation: {
+      nextEl: ".recipe__slider-next",
+      prevEl: ".recipe__slider-prev",
+    },
+    pagination: {
+      el: ".recipe__slider-pagination",
+      clickable: true,
+    },
+  });
+} catch (e) {}
+
+try {
+  // Show more Ingredients
+  const showMore = document.querySelectorAll(".r-item__ingredients-show-more");
+
+  showMore.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      const ingredientsWrapper = btn.previousElementSibling;
+      ingredientsWrapper.classList.toggle("active");
+
+      if (ingredientsWrapper.classList.contains("active")) {
+        btn.textContent = "Свернуть";
+      } else {
+        btn.textContent = "Развернуть";
+      }
+    });
+  });
+} catch (e) {}
+
+try {
+  // Show more Cooking
+  const showMore = document.querySelectorAll(".r-item__cooking-show-more");
+
+  showMore.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      const cookingText = btn.previousElementSibling;
+      cookingText.classList.toggle("active");
+
+      if (cookingText.classList.contains("active")) {
+        btn.textContent = "Свернуть";
+      } else {
+        btn.textContent = "Развернуть";
+      }
+    });
+  });
+} catch (e) {}
+
+try {
+  const tabDropdown = document.querySelectorAll(".r-item__dropdown");
+  const tabs = document.querySelectorAll(".r-item__tabs");
+  const tabItems = document.querySelectorAll(".tab-item");
+
+  tabDropdown.forEach((btn) => {
+    const btnText = btn.children[0];
+    btn.addEventListener("click", () => {
+      const tab = btn.nextElementSibling;
+      btn.classList.toggle("active");
+      tab.classList.toggle("active");
+    });
+
+    tabItems.forEach((item) => {
+      item.addEventListener("click", () => {
+        btnText.textContent = item.textContent;
+        btn.classList.remove("active");
+        tabs.forEach((tab) => tab.classList.remove("active"));
+      });
+    });
+  });
+} catch (e) {}
+
+try {
+  const buyBtn = document.querySelectorAll(".item__back-buy");
+  const buyModal = document.querySelector(".buy-modal");
+  const buyModalClose = buyModal.querySelector(".buy-modal__close");
+
+  buyBtn.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      document.body.classList.add("no-scroll");
+      buyModal.classList.add("buy-modal--active");
+    });
+  });
+
+  buyModalClose.addEventListener("click", () => {
+    document.body.classList.remove("no-scroll");
+    buyModal.classList.remove("buy-modal--active");
+  });
+} catch (e) {}
